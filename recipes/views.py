8b43de5edc117380b70c26c6269648222c7c5ba3
Claudio Junior <cjnetbr@hotmail.com>
2024-05-01 12:22:56 -0300

    Removido contao e sobre - Aula 27

--- recipes/views.py
+++ recipes/views.py
@@ -6,9 +6,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse("SOBRE")
