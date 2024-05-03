5801a840dbf874d8ad6426c4a4d645bc11377127
Claudio Junior <cjnetbr@hotmail.com>
2024-05-03 14:07:54 -0300

    Separando os templkates de home e recipes - Aula 43

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
